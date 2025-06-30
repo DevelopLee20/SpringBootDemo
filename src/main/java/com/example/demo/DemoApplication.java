ca22077ce100ac0c7f88fb937ad0d709e0dbc8b0
LeeInGyu <Developlee20@gmail.com>
2025-06-30 18:49:14 +0900

    Refactor: Edit file for readability

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-<<<<<<< HEAD
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-=======
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
->>>>>>> ccd73301370726594264e1f6f11f9f99ac7adb32
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,11 +33,7 @@ public class DemoApplication {
 // 이 클래스를 엔티티로 사용: 데이터베이스 테이블과 매핑된다는 의미의 JPA 어노테이션
 @Getter // 게터를 롬복에서 사용
 @Setter // 세터를 롬복에서 사용
-<<<<<<< HEAD
 @Entity
-=======
-//@NoArgsConstructor
->>>>>>> ccd73301370726594264e1f6f11f9f99ac7adb32
 class Coffee {
     // @Id DB 필드의 ID 표시
     @Id
@@ -92,23 +83,11 @@ class DataLoader {
 @RestController
 @RequestMapping("/coffees")
 class RestApiDemoController {
-<<<<<<< HEAD
-    //    private final List<Coffee> coffees = new ArrayList<>();
     private final CoffeeRepository coffeeRepository;
-=======
-    private List<Coffee> coffees = new ArrayList<>();
->>>>>>> ccd73301370726594264e1f6f11f9f99ac7adb32
 
     // 생성자
     public RestApiDemoController(CoffeeRepository coffeeRepository) {
         this.coffeeRepository = coffeeRepository;
-//        this.coffeeRepository.saveAll(List.of(
-//                new Coffee("Cafe1"),
-//                new Coffee("Cafe2"),
-//                new Coffee("Cafe3"),
-//                new Coffee("Cafe4")
-//        ));
-//        coffees.addAll(List.of(new Coffee("Cafe1"), new Coffee("Cafe2"), new Coffee("Cafe3"), new Coffee("Cafe4")));
     }
 
     // 반복 가능한(ex. 배열) coffee 객체 반환
@@ -117,61 +96,29 @@ class RestApiDemoController {
         return coffeeRepository.findAll();
     }
 
-    //    @RequestMapping(value = "/coffees", method = RequestMethod.GET) // 요청을 받는데, url과 method를 지정
-    // GET 요청만 호용해서 가독성이 좋아진다.
+    // GET 요청만 허용해서 가독성이 좋아진다.
     @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
         return coffeeRepository.findById(id);
-//        // 커피 아이디로 커피 검색 후 객체를 반환
-//        for (Coffee coffee : coffees) {
-//            if (coffee.getId().equals(id)) {
-//                return Optional.of(coffee);
-//            }
-//        }
-//        return Optional.empty();    // 없으면 빈 객체 반환(Optional 이라 빈 객체가 허용된다)
     }
 
-<<<<<<< HEAD
     @PostMapping()
-=======
-    @PostMapping
->>>>>>> ccd73301370726594264e1f6f11f9f99ac7adb32
     Coffee postCoffee(@RequestBody Coffee coffee) {
         return coffeeRepository.save(coffee);
-//        coffees.add(coffee); // coffee 추가
-//        return coffee;
     }
 
-//    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-<<<<<<< HEAD
-        // ResponseEntity<Coffee>를 반환하는 이유? 수정과 생성을 상태 코드로 명확히 구분하기 위해서
-=======
     @PutMapping("/{id}")
->>>>>>> ccd73301370726594264e1f6f11f9f99ac7adb32
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
         return (coffeeRepository.existsById(id))
                 ? new ResponseEntity<>(coffeeRepository.save(coffee),
                 HttpStatus.OK)
                 : new ResponseEntity<>(coffeeRepository.save(coffee),
                 HttpStatus.CREATED);
-//        int coffeeIdx = -1;
-//
-//        for (Coffee c : coffees) {
-//            if (c.getId().equals(id)) {
-//                coffeeIdx = coffees.indexOf(c);     // 찾았으면 인덱스 반환
-//                coffees.set(coffeeIdx, coffee);     // 커피 정보 업데이트
-//            }
-//        }
-//        // 만약 수정할 정보가 없으면 새로 추가
-//        return (coffeeIdx == -1) ?
-//                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-//                new ResponseEntity<>(coffee, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     void deleteCoffee(@PathVariable String id) {
         coffeeRepository.deleteById(id);
         // 커피를 검색해서 해당 아이디가 있으면 삭제 후 true 반환, 없으면 false를 반환
-//        coffees.removeIf(coffee -> coffee.getId().equals(id));
     }
 }
